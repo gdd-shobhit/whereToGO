49133483df61be707accd6df9f22ef57f0c1f02a
acm1551 <acm1551@IGM2350-16>
2020-02-24 17:40:12 -0500

    Grapple and Movement
    Grapple works.
    Cleaned up the code.
    Movement improved.

--- whereToGO/Assets/Scripts/playerMovement.cs
+++ whereToGO/Assets/Scripts/playerMovement.cs
@@ -2,11 +2,18 @@
 
 public class playerMovement : MonoBehaviour
 {
+    public GameObject grapplePrefab;
+    private GameObject grapple;
+    private float GRAPPLE_DISTANCE = 4.0f;
+    private Vector3 grappleDestination;
+
     public Rigidbody2D rb;
     public float speed=2f;
-    private int extraJumps=1;
+    private int jumps=2;
     public int extraJumpValue;
     private float movement=0f;
+    private bool isGrappling = false;
+    private bool grappleHit = false;
     public bool facingRight = true;
     public bool isGrounded;
     public bool isWalled;
@@ -25,83 +32,76 @@ public class playerMovement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        extraJumps = extraJumpValue;
+        grapple = Instantiate(grapplePrefab, transform.position, Quaternion.identity);
+        grapple.SetActive(false);
+        jumps = extraJumpValue;
         fireStance = false;
     }
 
     void FixedUpdate()
     {
-        movement = Input.GetAxis("Horizontal");
-        vel = rb.velocity;
-        //clampedVel = Vector2.ClampMagnitude(rb.velocity, -0.2f);
-        //rb.velocity = clampedVel;
-        rb.AddForce(new Vector2(movement * speed*25, rb.velocity.y), ForceMode2D.Force);
-
-        // checking for fire stance
-        if (Input.GetKeyDown(KeyCode.F))
+        if (!isGrappling)
         {
-            FireStance();
+            InitializeGrapple();
+            Movement();
         }
-        if (Input.GetKeyUp(KeyCode.F))
+        else
         {
-            gameObject.transform.GetChild(2).gameObject.SetActive(false);
+            ShootGrapple();
         }
-        //rb.transform.position = rb.transform.position+new Vector3(movement * speed * Time.deltaTime, rb.velocity.y, 0);
-        DeathMangager();
-        
-        if (movement>0 && facingRight == false)
-        {           
-            Flip();
-        }
-        else if(movement<0 && facingRight == true)
-        {          
-            Flip();
-        }
-        if (gameObject.transform.position.y < -15f)
-        {
-            gameObject.transform.position = new Vector3(0, 8, 1);
-        }
-    }
-    private void Update()
-    {
-        isGrounded = Physics2D.OverlapCircle(feetPos.position, feetRadius, whatIsGround);
-        isWalled= Physics2D.OverlapCircle(feetPos.position, feetRadius, whatIsWall);
-        if (isGrounded == true || isWalled==true )
-        {
-            extraJumps = extraJumpValue;
-            
-        }        
-        if(Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
-        {
-            //rb.velocity = transform.up * jumpForce * Time.deltaTime;
-            rb.AddForce(new Vector2(0, jumpForce*100f), ForceMode2D.Force);
-            extraJumps--;
-        }
-        else if(Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && (isGrounded == true || isWalled==true))
-        {
-            rb.AddForce(new Vector2(0, jumpForce * 100f), ForceMode2D.Force);
-            //skull.Play();
-        }
+        FireStance();
+        //DeathMangager();
 
     }
+
     void Flip()
     {
         facingRight = !facingRight;
         Vector3 Scaler = transform.localScale;
         Scaler.x *= -1;
         transform.localScale = Scaler;
-        //Instantiate(skull);
-        //skull.Play() ;
     }
+
+    void Movement()
+    {
+        movement = Input.GetAxis("Horizontal");
+        vel = rb.velocity;
+        rb.AddForce(new Vector2(movement * speed * 25, rb.velocity.y), ForceMode2D.Force);
+
+        isWalled = Physics2D.OverlapCircle(feetPos.position, feetRadius, whatIsWall);
+        RaycastHit2D hit = Physics2D.Raycast(feetPos.position, Vector2.down);
+        if (hit)
+            if (hit.rigidbody.gameObject.tag != "alive") isGrounded = hit.distance < 0.01f;
+
+        if (isGrounded || isWalled) jumps = extraJumpValue;
+        Debug.Log(hit.distance);
+        if (Input.GetKeyDown(KeyCode.W) && jumps > 0)
+        {
+            Debug.Log(jumps);
+            rb.AddForce(new Vector2(0, jumpForce * 100f), ForceMode2D.Force);
+            jumps--;
+        }
+
+        if (movement > 0 && facingRight == false) Flip();
+        else if (movement < 0 && facingRight == true) Flip();
+    }
+
     void DeathMangager()
     {
         if (gameObject.tag == "dead")
         {
             Vector3 bloodVector = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 1);
             Instantiate(deathBlood, bloodVector, Quaternion.identity);
-            Destroy(gameObject);            
+            Destroy(gameObject);
+        }
+
+        if (gameObject.transform.position.y < -15f)
+        {
+            gameObject.transform.position = new Vector3(0, 4, 1);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //if (collision.tag == "deathObjectf")
@@ -112,6 +112,57 @@ public class playerMovement : MonoBehaviour
 
     void FireStance()
     {
-        gameObject.transform.GetChild(2).gameObject.SetActive(true);
+        // checking for fire stance
+        if (Input.GetKeyDown(KeyCode.F)) 
+            gameObject.transform.GetChild(2).gameObject.SetActive(true);
+        if (Input.GetKeyUp(KeyCode.F))
+            gameObject.transform.GetChild(2).gameObject.SetActive(false);
+    }
+
+    //Initialize the grapple
+    void InitializeGrapple()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+            grappleDestination = mousePos - new Vector3(transform.position.x, transform.position.y, -1);
+            grappleDestination.Normalize();
+            grappleDestination = (grappleDestination * GRAPPLE_DISTANCE) + transform.position;
+            grapple.transform.position = transform.position;
+            grapple.SetActive(true);
+            isGrappling = true;
+            rb.gravityScale = 0;
+            rb.velocity *= 0.1f;
+        }
+    }
+
+    //Called while grappling
+    void ShootGrapple()
+    {
+        if (!grappleHit)
+        {
+            grapple.transform.position = Vector3.Lerp(grapple.transform.position, grappleDestination, 0.1f);
+            RaycastHit2D hit = Physics2D.Raycast(grapple.transform.position, grappleDestination, 0.05f);
+            if (hit)
+                if (hit.rigidbody.gameObject.tag != "alive")
+                    grappleHit = true;
+            if (Vector2.Distance(grappleDestination, grapple.transform.position) < 0.2f)
+                EndGrapple();
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, grappleDestination, 0.1f);
+            if (Vector2.Distance(grapple.transform.position, transform.position) < 0.2f)
+                EndGrapple();
+        }
+    }
+
+    private void EndGrapple()
+    {
+        isGrappling = false;
+        grappleHit = false;
+        grapple.SetActive(false);
+        rb.gravityScale = 1;
     }
 }
